fc830bd2a29e7855fe7bf20425b81df5794f42ff
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-11-29 14:20:20 +0600

    menu

---
+++ day-29/pages/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href="../assets/css/bootstrap.css"/>
+    <title>Day-29</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <a href="" class="navbar-brand">LOGO</a>
+            <ul class="navbar-nav">
+                <li><a href="" class="nav-link">Example One</a></li>
+                <li><a href="" class="nav-link">Example Two</a></li>
+                <li><a href="" class="nav-link">Example Three</a></li>
+                <li><a href="" class="nav-link">Example Four</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+
+
+
+<script src="../assets/js/jquery-3.6.0.js"></script>
+<script src="../assets/js/bootstrap.bundle.js"></script>
+<script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
