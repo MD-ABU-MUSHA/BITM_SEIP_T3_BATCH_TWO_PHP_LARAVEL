f6cfaf563a2a39fff9ead3d395f02d571b4c59bd
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-12-01 17:15:03 +0600

    image-store-and-name-change

--- day-31/app/classes/ImageUpload.php
+++ day-31/app/classes/ImageUpload.php
@@ -12,7 +12,9 @@ class ImageUpload
     protected $directory;
     protected  $fileType;
     protected  $check;
-    public $result = [];
+    protected $name;
+    protected $tmpName;
+
 
 
     public function __construct($data, $file)
@@ -29,10 +31,12 @@ class ImageUpload
 
 
 
-        $this->imageName = $this->file['image']['name'];
 
-      $this->directory = '../assets/img/upload/'.$this->imageName;
-      $this->fileType = pathinfo($this->imageName, PATHINFO_EXTENSION);
+        $this->imageName = $this->file['image']['name'];
+        $this->fileType = pathinfo($this->imageName, PATHINFO_EXTENSION);
+        $this->tmpName = str_replace(' ','_',$this->post['name']);
+        $this->name = $this->tmpName.'_'.time().'.'.$this->fileType;
+        $this->directory = '../assets/img/upload/'.$this->name;
 
         $this->check = getimagesize($this->file['image']['tmp_name']);
 
@@ -57,13 +61,5 @@ class ImageUpload
 
 
 
-
-//      move_uploaded_file($_FILES['image']['tmp_name'],$this->directory);
-
-//      $this->result = [
-//          'name' => $this->value['name'],
-//            'image' => $this->directory,
-//        ];
-//      return $this->result;
     }
 }
