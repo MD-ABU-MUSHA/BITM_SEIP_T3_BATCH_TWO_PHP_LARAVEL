58a20264c15f8ae85febe8af062b6e704f0a69a9
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-12-01 16:23:20 +0600

    image_uplod

--- day-31/app/classes/ImageUpload.php
+++ day-31/app/classes/ImageUpload.php
@@ -6,29 +6,64 @@ namespace App\classes;
 
 class ImageUpload
 {
-    public $value;
-    public $directory;
+    protected $post;
+    protected $imageName;
+    protected $file;
+    protected $directory;
+    protected  $fileType;
+    protected  $check;
     public $result = [];
 
 
-    public function __construct($data)
+    public function __construct($data, $file)
     {
-        $this->value = $data;
+        $this->post = $data;
+        $this->file = $file;
     }
 
     public function index(){
-        echo '<pre>';
-        print_r($this->value);
-        print_r($_FILES);
-        echo '</pre>';
+//        echo '<pre>';
+//        print_r($this->file);
+//        print_r($_FILES);
+//        echo '</pre>';
 
-    $this->directory = '../assets/img/upload/'.$_FILES['image']['name'];
-      move_uploaded_file($_FILES['image']['tmp_name'],$this->directory);
 
-      $this->result = [
-          'name' => $this->value['name'],
-            'image' => $this->directory,
-        ];
-      return $this->result;
+
+        $this->imageName = $this->file['image']['name'];
+
+      $this->directory = '../assets/img/upload/'.$this->imageName;
+      $this->fileType = pathinfo($this->imageName, PATHINFO_EXTENSION);
+
+        $this->check = getimagesize($this->file['image']['tmp_name']);
+
+        if($this->check)
+        {
+            if(!file_exists($this->directory))
+            {
+                if($this->fileType == 'jpg' || $this->fileType == 'png')
+                {
+                    move_uploaded_file($this->file['image']['tmp_name'],$this->directory);
+                }else{
+                    echo 'Faizlami Paisen JPG $ PNG File Den MIA....';
+                }
+            }else{
+                echo('MOZa lon Mia aki jinis amre duibar den ken....valo hoa jan');
+//         die('MOZa lon Mia aki jinis amre duibar den ken....valo hoa jan');
+//         exit('MOZa lon Mia aki jinis amre duibar den ken....valo hoa jan');
+            }
+        }else{
+            echo 'vai chobi den.... onno kisu diyen na';
+        }
+
+
+
+
+//      move_uploaded_file($_FILES['image']['tmp_name'],$this->directory);
+
+//      $this->result = [
+//          'name' => $this->value['name'],
+//            'image' => $this->directory,
+//        ];
+//      return $this->result;
     }
 }
