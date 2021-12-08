88051147833b52c6c6c015586c7da34a6498b481
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-12-08 14:12:32 +0600

    student

--- day-35/app/classes/Person.php
+++ day-35/app/classes/Person.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace App\classes;
@@ -40,4 +41,48 @@ class Person
         }
         return 'Student info save successfully';
     }
+=======
+<?php
+
+namespace App\classes;
+
+
+class Person
+{
+    private $name;
+    private $email;
+    private $mobile;
+    private $connection;
+    private $sql;
+    public $message;
+
+    public function  __construct($data = null)
+    {
+        if($data)
+        {
+            $this->name = $data['name'];
+            $this->email = $data['email'];
+            $this->mobile = $data['mobile'];
+        }
+    }
+
+
+    public function save()
+    {
+
+        $this->connection =  mysqli_connect('localhost', 'root', '', 'example_two');
+//      echo '<pre>';
+//      print_r($this->connection);
+//      echo '</pre>';
+
+        if($this->connection)
+        {
+            $this->sql = "INSERT INTO test(name, email, mobile) VALUES('$this->name', '$this->email', '$this->mobile')";
+            mysqli_query($this->connection, $this->sql);
+
+
+        }
+        return 'Student info save successfully';
+    }
+>>>>>>> fc39d5d62c053347a362956649b2119182328f20
 }
