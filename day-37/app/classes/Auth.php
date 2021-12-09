8a77a2b457eaba534a40d033bca9876e2ae8f8f1
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-12-09 16:19:11 +0600

    data_manage

---
+++ day-37/app/classes/Auth.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\classes;
+class Auth
+{
+    private $email;
+    private $password;
+    private $user;
+
+
+    public function __construct($data = null)
+    {
+        if($data)
+        {
+            $this->email = $data['email'];
+            $this->password = $data['password'];
+        }
+    }
+
+
+    public function index()
+    {
+        header('Location: pages/login.php');
+    }
+
+
+    public function login()
+    {
+
+    }
+
+
+    public function logout()
+    {
+
+    }
+
+
+    public function changePassword()
+    {
+
+    }
+}
